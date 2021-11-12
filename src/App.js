956d8fc971bc8292eb251380f60b1cc083a87c29
lf-devs <devs@learnforce.app>
2021-11-12 22:06:42 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import ImageSlider from "./components/ImageSlider";
+
+import {  LARGE_IMAGES } from "./data/data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+     
+      <div className='container'>
+        <ImageSlider images={LARGE_IMAGES} />
+      </div>
     </div>
   );
 }
---
+++ src/data/data.js
@@ -0,0 +1,34 @@
+
+
+export const LARGE_IMAGES = [
+  {
+    id: 1,
+    src: "https://media.discordapp.net/attachments/809815660682412063/908708314676875284/unknown.png?width=1202&height=676",
+    alt: "Placeholder image",
+  },
+  {
+    id: 2,
+    src: "https://media.discordapp.net/attachments/809815660682412063/908702812072980580/unknown.png?width=1202&height=676",
+    alt: "Placeholder image",
+  },
+  {
+    id: 3,
+    src: "https://media.discordapp.net/attachments/809815660682412063/908703902189359114/unknown.png?width=1202&height=676",
+    alt: "Placeholder image",
+  },
+  {
+    id: 4,
+    src: "https://media.discordapp.net/attachments/809815660682412063/908706142266458172/unknown.png?width=1202&height=676",
+    alt: "Placeholder image",
+  },
+  {
+    id: 5,
+    src: "https://media.discordapp.net/attachments/809815660682412063/908707219170803743/unknown.png?width=1202&height=676",
+    alt: "Placeholder image",
+  },
+  {
+    id: 6,
+    src: "https://media.discordapp.net/attachments/809815660682412063/908708314676875284/unknown.png?width=1202&height=676",
+    alt: "Placeholder image",
+  },
+];
